wine-mirror/wine

commit 85d029e3b01f6dd35a86cc07796af982d66e4a03

shell32: Rely solely on SHELL_FindExecutable for ShellExecute.
This does two things:

1. ShellExecute no longer searches in the directory where the current executable is in.
2. We always CreateProcess with a fully qualified path, so we CreateProcess won't do its own path
   resolution.

Serendipitously this also fixes some existing todos in tests.

dlls/shell32/shlexec.c
@@ -1823,30 +1823,8 @@ static BOOL SHELL_execute( LPSHELLEXECUTEINFOW sei, SHELL_ExecuteW32 execfunc )
         wcmd = malloc(len * sizeof(WCHAR));
         wcmdLen = len;
     }
-    lstrcpyW(wcmd, wszApplicationName);
-    if (sei_tmp.lpDirectory)
-    {
-        LPCWSTR searchPath[] = {
-            sei_tmp.lpDirectory,
-            NULL
-        };
-        PathFindOnPathW(wcmd, searchPath);
-    }
-    retval = SHELL_quote_and_execute( wcmd, wszParameters, L"",
-                                      wszApplicationName, NULL, &sei_tmp,
-                                      sei, execfunc );
-    if (retval > 32) {
-        free(wszApplicationName);
-        if (wszParameters != parametersBuffer)
-            free(wszParameters);
-        if (wszDir != dirBuffer)
-            free(wszDir);
-        if (wcmd != wcmdBuffer)
-            free(wcmd);
-        return TRUE;
-    }
 
-    /* Else, try to find the executable */
+    /* try to find the executable */
     wcmd[0] = '\0';
     retval = SHELL_FindExecutable(sei_tmp.lpDirectory, lpFile, sei_tmp.lpVerb, wcmd, wcmdLen, wszKeyname, &env, sei_tmp.lpIDList, sei_tmp.lpParameters);
     if (retval > 32)  /* Found */

dlls/shell32/tests/shlexec.c
@@ -2331,9 +2331,7 @@ static void test_exes(void)
         if (CopyFileA(argv0, filename, FALSE))
         {
             rc=shell_execute(NULL, filename, params, NULL);
-            todo_wine {
-                okShell(rc==SE_ERR_NOASSOC, "returned %Iu\n", rc);
-            }
+            okShell(rc==SE_ERR_NOASSOC, "returned %Iu\n", rc);
         }
     }
     else
@@ -2359,7 +2357,7 @@ static void test_exes(void)
     GetCurrentDirectoryA(MAX_PATH, curdir);
     SetCurrentDirectoryA(tmpdir);
     rc = shell_execute(NULL, basename, params, NULL);
-    todo_wine okShell(rc == SE_ERR_FNF, "returned %Iu\n", rc);
+    okShell(rc == SE_ERR_FNF, "returned %Iu\n", rc);
     SetCurrentDirectoryA(curdir);
 
     if (!skip_shlexec_tests)
@@ -2956,7 +2954,7 @@ static void test_directory(void)
                         NULL, "test2.exe", params, NULL, NULL);
     okShell(rc > 32, "returned %Iu\n", rc);
     okChildInt("argcA", 4);
-    todo_wine okChildString("argvA0", path);
+    okChildString("argvA0", path);
     okChildString("argvA3", "Exec");
     okChildPath("longPath", path);
     SetCurrentDirectoryA(curdir);

metux/wine-hacks

commit c9162badbca91ac71d04e7b20071d611245d25ae

shell32: Restore the ability of running unix executables via unix paths
MobaXTerm tries to ShellExecute "/bin/sh" when running under wine. This ability
to run unix executables on the host via unix paths was accidentally removed
in the attemp to align ShellExecute more closely with native, while fixing the
problem that sometimes wrong executable is started by ShellExecute (e.g. in
Motor Race Collection).

Related: a2548c8db3096963012939c82e340f6b867f3efd
Fixes: 85d029e3b01f6dd35a86cc07796af982d66e4a03

dlls/shell32/shlexec.c
@@ -1934,16 +1934,26 @@ static BOOL SHELL_execute( LPSHELLEXECUTEINFOW sei, SHELL_ExecuteW32 execfunc )
         lstrcatW(lpstrTmpFile, lpFile);
         retval = (UINT_PTR)ShellExecuteW(sei_tmp.hwnd, sei_tmp.lpVerb, lpstrTmpFile, NULL, NULL, 0);
     }
-    else if (retval == SE_ERR_NOASSOC && SHGetFileInfoW(wcmd, 0, NULL, 0, SHGFI_EXETYPE) == 0)
+    else if ((retval == SE_ERR_NOASSOC && SHGetFileInfoW(wcmd, 0, NULL, 0, SHGFI_EXETYPE) == 0) ||
+             (retval == SE_ERR_FNF && wszApplicationName[0] == '/'))
     {
-        /* File found, but no association. And no other cases fit, this could be a
+        /* Two cases:
+
+           1. File found, but no association. And no other cases fit, this could be a
            unix programs, try running it. We have to do this in a "catch-all" fashion because
            unix program can have any extensions. However, things get more complicated because
            the file we find could be a Windows executable without the proper extensions, it could
-           be seen as unexpected if we start it, so we special case it here. */
+           be seen as unexpected if we start it, so we special case it here.
+
+           2. File not found. Still, this could be a unix path. In this case we don't worry if
+           we accidentally start a Windows executable, so we just try to run it anyway, hoping it's
+           a valid unix path to a executable. Also `wcmd` will be empty in this case, so we use
+           `wszApplicationName`.
+         */
         UINT exec_retval;
-        TRACE("No association found, trying as Unix binary %s\n", debugstr_w(wcmd));
-        exec_retval = SHELL_quote_and_execute( wcmd, wszParameters, wszKeyname,
+        WCHAR *unix_cmd = retval == SE_ERR_NOASSOC ? wcmd : wszApplicationName;
+        TRACE("SHELL_FindExecutable failed, trying as Unix binary %s\n", debugstr_w(unix_cmd));
+        exec_retval = SHELL_quote_and_execute( unix_cmd, wszParameters, wszKeyname,
                                                wszApplicationName, env, &sei_tmp,
                                                sei, execfunc );
         TRACE("Unix binary returned %u\n", exec_retval);